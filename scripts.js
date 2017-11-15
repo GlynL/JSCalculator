6f903ffc10d824e794b95aa804813aef9bf45c36
Glyn L <glyn_92@hotmail.com>
2017-11-15 16:41:24 +0700

    completed... for now

--- scripts.js
+++ scripts.js
@@ -1,11 +1,10 @@
 var buttons = document.querySelectorAll('button');
 var input = document.querySelector('#input');
-var value = input.textContent;
-var valueStore  ;
+var value = "0";
+var valueStore = 0;
 var operator; /* undefined */
-var newCalc = true;
+var operators = ["%", "AC", "+", "-", "/", "*", "="]
 var chaining = false;
-var operators = ["C", "%", "+", "-", "X", "÷"]
 
 //maybe add object for key events
 
@@ -13,123 +12,71 @@ buttons.forEach(function(button){
     button.addEventListener('click', calculateField);
 });
 
+// clearing and resetting
+function clearField(){
+    input.textContent = "0";
+    value = "0";
+    valueStore = 0;
+    operator = undefined;
+}
+
+// when an operator key is pressed
+function operatorsLogic(keyValue){
+    if(operator === undefined){
+        operator = keyValue;
+        valueStore = value;
+        value = operator;
+    }
+
+    else if(operator){
+        valueStore = valueStore + operator + value;
+        operator = keyValue;
+        value = operator;
+    }  
+
+    if(keyValue === "="){
+        calculateTotal();
+        operator = "=";
+    }
+
+    if(keyValue === "AC"){
+        clearField();
+    }
+    
+}
+
+// logic for '=' calc
+function calculateTotal(){
+    value = eval(valueStore);
+
+}
+
 function calculateField(){
     // what key was pressed
     var keyValue = this.textContent;
-    
-    //if it's a new calulation
-    if(newCalc){
+
+    // if the last button pressed was an operator set value to "0" so that logic works
+    if(operators.includes(input.textContent) ){
         value = "0";
+    }
+
+    if(operator === "="){
+        clearField();
     }
 
     //if there's no value in input field and it's not an operator key
-    if(value === "0" && operators.indexOf(keyValue) === -1 && keyValue !== "="){
-        value = keyValue;
-        newCalc = false;
-    }
-
-    else if(chaining === true && operators.indexOf(keyValue)=== -1 && keyValue !== '='){
+    if(value === "0" && !operators.includes(keyValue)){
         value = keyValue;
     }
-
-    // below all operators & '='
-
-    //if clear button clicked
-    else if(keyValue === "C"){
-        value = "0";
-        valueStore = undefined;
-        operator = undefined;
-        newCalc = true;
-        chaining = false;
-    }
-
-    //if dividing
-    else if(keyValue === "÷"){
-        valueStore = value;
-        operator = "÷";
-        value = "";
-    }
-
-    // if multiplying
-    else if(keyValue === "X"){
-        valueStore = value;
-        operator = "X";
-        value = "";
-    }
-
-    // if subtracting
-    else if(keyValue ==="-"){
-        if(chaining){
-            value = valueStore - value;
-            valueStore = value;
-        }
-        else{
-            valueStore = value;
-            value = "";
-            chaining =true;
-        }
-
-        operator = "-";
-        
-    }
-
-    //if adding
-    else if(keyValue === "+"){
-        if(chaining){
-            value = Number(value) + Number(valueStore);    
-            valueStore = value;     
-        }
-        else{
-            valueStore = value;
-            value = "";
-            chaining = true;
-        }   
-        operator = "+";   
-    }
-        
     
+    else if(operators.includes(keyValue)){
+        operatorsLogic(keyValue);
+    }          
 
-    // if %
-    else if(keyValue === "%"){
-        value = value/100;
-        newCalc = true;
-        operator = "%";
+    else if(keyValue === "+/-"){
+        value = value * -1;
     }
 
-    // if +/-
-    else if(keyValue ==="+/-"){
-        value = value*-1;
-    }
-
-    // calculating
-    else if(keyValue === "="){
-        
-        if(operator === undefined){
-            value = "0"; /* do we need? */
-        }
-        // if dividing
-        else if(operator === "÷"){
-            value = valueStore/value;
-        }
-        //if multiplying
-        else if(operator === "X"){
-            value = valueStore*value;
-        }
-
-        // if subtracting
-        else if(operator ==="-"){
-            value = valueStore - value;
-        }
-
-        // if adding
-        else if(operator === "+"){
-            value = Number(valueStore) + Number(value);
-        }
-    }
-
-    // above all operators & '='
-
-    //for inputting 2+ digits
     else{
         value += keyValue;
     }
@@ -137,13 +84,3 @@ function calculateField(){
     // all equations adjust the input to 'value'
     input.textContent = value;
 }
-
-
-// make chaining equations work
-// converting to number?
-
-
-// can we create a function for the operators in calculating section
-    // valuestore + operator + value ????
-    // operator needs to be a value and not a string
-
